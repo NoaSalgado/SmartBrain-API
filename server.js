df0b00f50808c4297d7f5db87320d766862d7f29
Noa <noasalgadodev@gmail.com>
2022-12-24 13:39:55 +0100

    Add API endpoints

--- server.js
+++ server.js
@@ -2,19 +2,96 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'Jhon',
+      email: 'jhon@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'jhon@gmail.com',
+    },
+  ],
+};
+
 app.get('/', (req, res) => {
   res.send('This is working');
+});
+
+app.post('/signin', (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('success');
+  } else {
+    res.status(400).json('Error login in');
+  }
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+
+  if (!found) {
+    res.status(404).json('No such user');
+  }
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+
+  if (!found) {
+    res.status(400).json('Not found');
+  }
 });
 
 app.listen(3000, () => {
   console.log('App is running!!');
 });
-
-/*
-ENPOINTS:
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userid --> GET = user
-/image --> PUT --> user
-*/
